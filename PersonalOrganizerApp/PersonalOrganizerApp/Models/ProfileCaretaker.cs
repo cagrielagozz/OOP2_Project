cb68fc1b4c875fbd83a9e0e06b8547e0ed70a798
Buğra Ayrancı <bugraayranci55@gmail.com>
2025-05-05 21:17:16 +0300

    Add files via upload

---
+++ PersonalOrganizerApp/PersonalOrganizerApp/Models/ProfileCaretaker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using PersonalOrganizerApp.Models;
+
+namespace PersonalOrganizerApp.Models
+{
+    public class ProfileCaretaker
+    {
+        private Stack<User> undoStack = new Stack<User>();
+        private Stack<User> redoStack = new Stack<User>();
+
+        public void SaveState(User user)
+        {
+            undoStack.Push(user.Clone());
+            redoStack.Clear();
+        }
+
+        public User Undo(User currentUser)
+        {
+            if (undoStack.Count == 0) return null;
+            redoStack.Push(currentUser.Clone());
+            User previous = undoStack.Pop();
+            currentUser.CopyFrom(previous);
+            return previous;
+        }
+
+        public User Redo(User currentUser)
+        {
+            if (redoStack.Count == 0) return null;
+            undoStack.Push(currentUser.Clone());
+            User next = redoStack.Pop();
+            currentUser.CopyFrom(next);
+            return next;
+        }
+    }
+
+}
+
---
+++ PersonalOrganizerApp/PersonalOrganizerApp/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalOrganizerApp.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Base64Photo { get; set; }
+
+        public User Clone()
+        {
+            return new User
+            {
+                Username = this.Username,
+                Name = this.Name,
+                Surname = this.Surname,
+                PhoneNumber = this.PhoneNumber,
+                Address = this.Address,
+                Email = this.Email,
+                Password = this.Password,
+                Base64Photo = this.Base64Photo
+            };
+        }
+
+        public void CopyFrom(User other)
+        {
+            Username = other.Username;
+            Name = other.Name;
+            Surname = other.Surname;
+            PhoneNumber = other.PhoneNumber;
+            Address = other.Address;
+            Email = other.Email;
+            Password = other.Password;
+            Base64Photo = other.Base64Photo;
+        }
+    }
+
+}
